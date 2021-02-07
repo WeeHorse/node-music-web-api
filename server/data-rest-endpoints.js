1bca63f463bb3a11ccf5674814f2fed32aee4070
Benjamin Berglund <benjaminberglund@gmail.com>
2021-02-07 03:27:28 +0100

    added example endpoints for CRUD

--- server/data-rest-endpoints.js
+++ server/data-rest-endpoints.js
@@ -1,44 +1,104 @@
 module.exports = (app, db) => {
 
+  // Authentication routes
 
   // authentication: perform login
-app.post('/api/login', async (request, response) => {
-  let user = await db.query('SELECT * FROM users WHERE email = ? AND password = ?', [request.body.email, request.body.password])
-  user = user[0]
-
-  if(user && user.email){
-    request.session.user = user
-    user.loggedIn = true
-    response.json({loggedIn: true})
-  }else{
-    response.status(401) // unauthorized  https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-    response.json({message:"no matching user"})
-  }
-})
-
-// authentication: get logged in user
-app.get('/api/login', async (request, response) => {
-  let user
-  if(request.session.user){
-    user = await db.query('SELECT * FROM users WHERE email = ? AND password = ?', [request.session.user.email, request.session.user.password])
+  app.post('/api/login', async (request, response) => {
+    let user = await db.query('SELECT * FROM users WHERE email = ? AND password = ?', [request.body.email, request.body.password])
     user = user[0]
-  }
-  if(user && user.email){
-    user.loggedIn = true
-    delete(user.password)
-    response.json(user)
-  }else{
-    response.status(401) // unauthorized  https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-    response.json({message:"not logged in"})
-  }
-})
 
-// logga ut
-app.delete('/api/login', async (request, response) => {
-  request.session.destroy( () => {
-    response.json({loggedIn: false})
-  } )
-})
+    if(user && user.email){
+      request.session.user = user
+      user.loggedIn = true
+      response.json({loggedIn: true})
+    }else{
+      response.status(401) // unauthorized  https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+      response.json({message:"no matching user"})
+    }
+  })
+
+  // authentication: get logged in user
+  app.get('/api/login', async (request, response) => {
+    let user
+    if(request.session.user){
+      user = await db.query('SELECT * FROM users WHERE email = ? AND password = ?', [request.session.user.email, request.session.user.password])
+      user = user[0]
+    }
+    if(user && user.email){
+      user.loggedIn = true
+      delete(user.password)
+      response.json(user)
+    }else{
+      response.status(401) // unauthorized  https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+      response.json({message:"not logged in"})
+    }
+  })
+
+  // logga ut
+  app.delete('/api/login', async (request, response) => {
+    request.session.destroy( () => {
+      response.json({loggedIn: false})
+    } )
+  })
+
+
+  // Example routes
+
+  // public get all table rows
+  app.get('/api/examples', async (request, response) => {
+    let data = await db.query('SELECT * FROM examples')
+    response.json(data)
+  })
+
+  // public get one table row
+  app.get("/api/examples/:id", async (request, response) => {
+    let data = await db.query("SELECT * FROM examples WHERE id = ?", [request.params.id])
+    data = data[0] // single row
+    response.json(data)
+  })
+
+  // public get one table row
+  app.get("/api/examples/:id", async (request, response) => {
+    let data = await db.query("SELECT * FROM examples WHERE id = ?", [request.params.id])
+    data = data[0] // single row
+    response.json(data)
+  })
+
+  // private create one row
+  app.post("/api/examples", async (request, response) => {
+    // check if user exists before writing
+    if(!request.session.user){
+      response.status(403) // forbidden
+      response.json({error:'not logged in'})
+      return;
+    }
+    let result = await db.query("INSERT INTO examples SET ?", request.body)
+    response.json(result)
+  })
+
+  // private update one row
+  app.put("/api/examples/:id", async (request, response) => {
+    // check if user exists before writing
+    if(!request.session.user){
+      response.status(403) // forbidden
+      response.json({error:'not logged in'})
+      return;
+    }
+    let result = await db.query("UPDATE examples SET ? WHERE id = ?", [request.body, request.params.id] )
+    response.json(result)
+  })
+
+  // private delete one row
+  app.delete("/api/examples/:id", async (request, response) => {
+    // check if user exists before writing
+    if(!request.session.user){
+      response.status(403) // forbidden
+      response.json({error:'not logged in'})
+      return;
+    }
+    let result = await db.query("DELETE FROM examples WHERE id = ?", request.params.id)
+    response.json(result)
+  })
 
 
 }
